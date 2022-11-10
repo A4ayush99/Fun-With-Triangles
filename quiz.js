7300c62bc5fcdce24308d729471dd03d50d3edbf
Aayush Namdev <45045225+A4ayush99@users.noreply.github.com>
2022-11-10 11:41:08 +0530

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,19 @@
+const quizForm = document.querySelector("#quiz-form");
+const submitBtn = document.querySelector("#submit-btn");
+const outputBox = document.querySelector("#output-box");
+
+const correctAnswers = ["90°", "right angled", "4.5cm", "10cm", "10√2 cm"];
+
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formResults = new FormData(quizForm);
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index++;
+  }
+  outputBox.innerText = `Your Score is ${score}`;
+}
+submitBtn.addEventListener("click", calculateScore);
